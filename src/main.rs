cfe9e8bbef5e53c4ba22d33612a5bb79fd9873df
Noskcaj <kcajdev@gmail.com>
2018-10-31 19:27:45 -0400

    Insert last result when an operator is entered

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate lalrpop_util;
 extern crate liner;
 extern crate slide;
+extern crate termion;
 
-use liner::Context;
+use liner::{Context, Event, EventKind};
+use termion::event::Key;
 
 use slide::{ast, calc, eval};
 
@@ -56,12 +58,45 @@ fn print_parse_error<'input>(error: ast::TParseError, input: &str) {
     )
 }
 
+// TODO: Currently a hack
+fn handle_event<W: std::io::Write>(event: Event<W>) {
+    if event.editor.cursor() != 0 {
+        return;
+    }
+    let result = {
+        let ctx = event.editor.context();
+        let last_expr = match ctx.history.buffers.get(ctx.history.len().saturating_sub(1)) {
+            Some(l) => l,
+            None => return,
+        };
+        let input = &last_expr.lines()[0];
+        let mut errors = Vec::new();
+        let expr = match calc::ExprParser::new().parse(&mut errors, input) {
+            Ok(e) => e,
+            Err(_) => return,
+        };
+        if errors.len() != 0 {
+            return;
+        }
+        eval::eval(*expr)
+    };
+    match event.kind {
+        EventKind::BeforeKey(Key::Char(ch)) => match ch {
+            '>' | '<' | '-' | '+' | '*' | '(' | '/' => {
+                let _ = event.editor.insert_str_after_cursor(&format!("{}", result));
+            }
+            _ => {}
+        },
+        _ => {}
+    };
+}
+
 fn main() {
     let mut con = Context::new();
     let mut input;
 
     loop {
-        input = match con.read_line("> ", &mut |_| {}) {
+        input = match con.read_line("> ", &mut handle_event) {
             Ok(line) => line,
             Err(ref e) if e.kind() == std::io::ErrorKind::Interrupted => {
                 // ctrl-c
