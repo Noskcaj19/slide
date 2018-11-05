b6f6ee1feeb22458949e9b4e10574094b9f28be6
Noskcaj <kcajdev@gmail.com>
2018-11-04 23:41:45 -0500

    Add missing operator error check

--- src/error.rs
+++ src/error.rs
@@ -4,5 +4,14 @@ pub fn try_humanize(error: &crate::ast::TErrorRecovery) -> Option<String> {
     if let UnrecognizedToken { token: None, .. } = error.error {
         return Some("Unexpected end of input".to_owned());
     }
+    if let UnrecognizedToken {
+        token: Some(_),
+        ref expected,
+    } = error.error
+    {
+        if expected.contains(&"\"operator\"".to_owned()) {
+            return Some("Possible missing operator".to_owned());
+        }
+    }
     None
 }
